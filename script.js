b1efe284de14a08de73df6ba96ad3fa2992bfd63
Antoine Chassagne <antoine.chassagne@protonmail.com>
2018-10-17 17:37:01 +0200

    improve size and smooth

--- script.js
+++ script.js
@@ -23,15 +23,19 @@ function generate() {
         var angulo = 360 / total * index * Math.PI / 180;
         var hue = angulo / (Math.PI * 2) * 360;
         var col = 'hsl(' + hue + ',90%, 50%)';
-
+        var dx = 250 + Math.cos(angulo) * 200;
+        var dy = 250 + Math.sin(angulo) * 200;
         // generation
         var part = {
             posx: 250,
             posy: 250,
-            vit: 1 + Math.random() * 100,
-            destx: 250 + Math.cos(angulo) * 200,
-            desty: 250 + Math.sin(angulo) * 200,
-            couleur: col
+            vit: 1 + Math.random() * 50,
+            destx: dx,
+            desty: dy,
+            o_destx: dx,
+            o_desty: dy,
+            couleur: col,
+            timer: 0
 
         }
         tab_part.push(part);
@@ -40,16 +44,24 @@ function generate() {
 
 function loop() {
 
-    c.clearRect(0, 0, 500, 500);
+    // c.clearRect(0, 0, 500, 500);
+    c.fillStyle = 'rgba(255,255,255,.25)';
+    c.rect(0, 0, 500, 500);
+    c.fill();
 
     for (var index = 0; index < tab_part.length; index++) {
 
         var currentpart = tab_part[index];
 
+        var originDistX = currentpart.o_destx - currentpart.posx;
+        var originDistY = currentpart.o_desty - currentpart.posy;
+        var originDistance = Math.sqrt((originDistX * originDistX) + (originDistY * originDistY));
+        var d = 2 / (1 + originDistance / 100);
+
         // affichage
         c.fillStyle = currentpart.couleur;
         c.beginPath();
-        c.ellipse(currentpart.posx, currentpart.posy, 2, 2, 0, 0, 2 * Math.PI);
+        c.ellipse(currentpart.posx, currentpart.posy, d, d, 0, 0, 2 * Math.PI);
         c.fill();
 
         // deplacement
@@ -61,14 +73,26 @@ function loop() {
         var distancey = mouseY - currentpart.posy;
         var distance = Math.sqrt((distancex * distancex) + (distancey * distancey));
 
+
         if (distance < 50) {
 
             var repulseangle = Math.atan2((currentpart.posy - mouseY), (currentpart.posx - mouseX));
-            var newDestX = currentpart.posx + Math.cos(repulseangle) * currentpart.vit*5;
-            var newDestY = currentpart.posy + Math.sin(repulseangle) * currentpart.vit*5;
+            var newDestX = currentpart.posx + Math.cos(repulseangle) * currentpart.vit * 5;
+            var newDestY = currentpart.posy + Math.sin(repulseangle) * currentpart.vit * 5;
 
             currentpart.destx = newDestX;
             currentpart.desty = newDestY;
+            currentpart.timer = Math.round(50 + Math.random() * 150);
+        }
+
+        // gestion timer
+        if (currentpart.timer > 0) {
+            currentpart.timer--;
+        }
+
+        if (currentpart.timer == 0) {
+            currentpart.destx = currentpart.o_destx;
+            currentpart.desty = currentpart.o_desty;
         }
 
     }
